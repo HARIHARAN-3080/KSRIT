39f49f9fa645bf5c6cdfc5aa45b295ff5fe0ee71
HARIHARAN-3080 <m.hariharan3080@gmail.com>
2025-05-24 16:20:00 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/KSRIT/client/',  // <--- Add this line, match your repo and folder path
+  base: '/KSRIT/',   // <-- base path matches your repo name
   plugins: [react(), tailwindcss()],
 })

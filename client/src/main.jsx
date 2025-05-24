7ed4ce23669bb5731a746e781843e7d4f0330ee4
HARIHARAN-3080 <m.hariharan3080@gmail.com>
2025-05-24 17:10:11 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-const Main = () => {
-  return <App />;
-};
+import { HashRouter } from 'react-router-dom';  // <-- Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <HashRouter>   {/* <-- Use HashRouter instead of BrowserRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
